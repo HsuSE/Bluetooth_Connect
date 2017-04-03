09edb43c16fe3d8b8625250fde6031b946e1e803
CengBoYan <brian260484@gmail.com>
2017-04-03 16:04:09 +0800

    Initial Commit

---
+++ Bluetooth_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bluetooth_Test
+//
+//  Created by NKMU.EDU on 2017/4/3.
+//  Copyright © 2017年 NKMU.EDU.shion.ble_test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
